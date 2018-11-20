b3efbd87bfb142945b26dede01e78a2b47e43e51
Dams <damien.nivault@supinternet.fr>
2018-11-20 16:26:58 +0100

    :sparkles: finished the register form

--- app/AdvancedTicTacToe/AdvancedTicTacToe/ViewController.swift
+++ app/AdvancedTicTacToe/AdvancedTicTacToe/ViewController.swift
@@ -32,13 +32,66 @@ extension UIViewController {
     }
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
 
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.hideKeyboardWhenTappedAround()
         
+        let username = UITextField()
+        username.placeholder = "Username"
+        username.backgroundColor = UIColor.white
+        username.layer.cornerRadius = 5
+        username.alpha = 0.7;
+        username.setLeftPaddingPoints(15)
+        username.keyboardAppearance = UIKeyboardAppearance.dark;
+        username.returnKeyType = UIReturnKeyType.continue
+        username.delegate = self
+        username.tag = 1
+        self.view.addSubviewGrid(username, grid: [1, 3, 10, 0.75])
+        
+        let labelEmail = UITextField()
+        labelEmail.placeholder = "Email"
+        labelEmail.backgroundColor = UIColor.white
+        labelEmail.layer.cornerRadius = 5
+        labelEmail.alpha = 0.7;
+        labelEmail.setLeftPaddingPoints(15)
+        labelEmail.keyboardAppearance = UIKeyboardAppearance.dark;
+        labelEmail.keyboardType = UIKeyboardType.emailAddress
+        labelEmail.returnKeyType = UIReturnKeyType.continue
+        labelEmail.delegate = self
+        labelEmail.tag = 2
+        self.view.addSubviewGrid(labelEmail, grid: [1, 4, 10, 0.75])
+        
+        let labelPassword = UITextField()
+        
+        labelPassword.placeholder = "Password"
+        labelPassword.backgroundColor = UIColor.white
+        labelPassword.layer.cornerRadius = 5
+        labelPassword.alpha = 0.7;
+        labelPassword.isSecureTextEntry = true
+        labelPassword.setLeftPaddingPoints(15)
+        labelPassword.keyboardAppearance = UIKeyboardAppearance.dark;
+        labelPassword.returnKeyType = UIReturnKeyType.continue
+        labelPassword.delegate = self
+        labelPassword.tag = 3
+        self.view.addSubviewGrid(labelPassword, grid: [1, 5, 10, 0.75])
+     
+        let labelVerifPassword = UITextField()
+        labelVerifPassword.placeholder = "Password Verification"
+        labelVerifPassword.backgroundColor = UIColor.white
+        labelVerifPassword.layer.cornerRadius = 5
+        labelVerifPassword.alpha = 0.9;
+        labelVerifPassword.isSecureTextEntry = true
+        labelVerifPassword.setLeftPaddingPoints(15)
+        labelVerifPassword.keyboardAppearance = UIKeyboardAppearance.dark;
+        labelVerifPassword.returnKeyType = UIReturnKeyType.join
+        labelVerifPassword.delegate = self
+        labelVerifPassword.tag = 4
+        self.view.addSubviewGrid(labelVerifPassword, grid: [1, 6, 10, 0.75])
         let button = UIButton()
         button.backgroundColor = UIColor(red: 51.0/255, green: 153.0/255, blue: 255.0/255, alpha: 0.9)
         button.setTitle("Register", for: .normal)
