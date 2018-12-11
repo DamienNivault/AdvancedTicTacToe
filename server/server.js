6ca9cc8fdcc73ae588c5182a7555dd1a9ede03cd
Anthony Spault <anthony@spault.fr>
2018-12-11 15:11:34 +0100

    test socketio

--- server/server.js
+++ server/server.js
@@ -42,67 +42,67 @@ app.use(errorHandler);
 var onlinePlayers = {}
 
 io.on('connection', function (socket) {
-    socket.on("movement", function (index) {
-        var game = games[socket.gamename]
-        if (game) {
-            if (game.turn === socket.wording) {
-                var result = game.move(index);
-                if (!result.err) {
-                    io.to(socket.gamename).emit('movement', {
-                        index: index,
-                        grid: game.grid,
-                        win: game.win,
-                        equality: game.equality,
-                        player_played: game.turn,
-                        player_play: game.getNextTurn(),
-                        err: null
-                    });
-                    game.changeTurn()
-                    if (game.win) {
-                        game.players['x'].leave(game.name)
-                        game.players['o'].leave(game.name)
-                        game.players['x'].game = null
-                        game.players['o'].game = null
-                    }
-                } else {
-                    io.to(games[socket.gamename].name).emit('movement', {
-                        err: 'wrong_movement'
-                    })
-                }
-            } else {
-                io.to(games[socket.gamename].name).emit('movement', {
-                    err: 'not_your_turn'
-                })
-            }
-        } 
-    });
+    // socket.on("movement", function (index) {
+    //     var game = games[socket.gamename]
+    //     if (game) {
+    //         if (game.turn === socket.wording) {
+    //             var result = game.move(index);
+    //             if (!result.err) {
+    //                 io.to(socket.gamename).emit('movement', {
+    //                     index: index,
+    //                     grid: game.grid,
+    //                     win: game.win,
+    //                     equality: game.equality,
+    //                     player_played: game.turn,
+    //                     player_play: game.getNextTurn(),
+    //                     err: null
+    //                 });
+    //                 game.changeTurn()
+    //                 if (game.win) {
+    //                     game.players['x'].leave(game.name)
+    //                     game.players['o'].leave(game.name)
+    //                     game.players['x'].game = null
+    //                     game.players['o'].game = null
+    //                 }
+    //             } else {
+    //                 io.to(games[socket.gamename].name).emit('movement', {
+    //                     err: 'wrong_movement'
+    //                 })
+    //             }
+    //         } else {
+    //             io.to(games[socket.gamename].name).emit('movement', {
+    //                 err: 'not_your_turn'
+    //             })
+    //         }
+    //     } 
+    // });
 
-    socket.on("join_game", function (data) {
-        socket.playerName = data;
-        socket.gamename = null;
-        var found = false
-        for (var i = 0; i < onlinePlayers.length; i++) {
-            if (onlinePlayers[i].playerName == data) {
-                found = true
-            }
-        }
-        if (!found) {
-          if (data.length < 12) {
-                console.log(data + " -> Join queue")
-                io.sockets.emit("log", data + " -> Join queue")
-            }
-          onlinePlayers.push(socket);
-        }
+    // socket.on("join_game", function (data) {
+    //     socket.playerName = data;
+    //     socket.gamename = null;
+    //     var found = false
+    //     for (var i = 0; i < onlinePlayers.length; i++) {
+    //         if (onlinePlayers[i].playerName == data) {
+    //             found = true
+    //         }
+    //     }
+    //     if (!found) {
+    //       if (data.length < 12) {
+    //             console.log(data + " -> Join queue")
+    //             io.sockets.emit("log", data + " -> Join queue")
+    //         }
+    //       onlinePlayers.push(socket);
+    //     }
 
-    });
+    // });
 
-    socket.on("leave_game", function (data) {
-        leaveGame(socket);
-    });
+    // socket.on("leave_game", function (data) {
+    //     leaveGame(socket);
+    // });
 
-    socket.on('disconnect', function () {
-        leaveGame(socket);
-    });
+    // socket.on('disconnect', function () {
+    //     leaveGame(socket);
+    // });
 
     socket.on('join_server', function(token) {
         // Token decrypte
@@ -114,10 +114,10 @@ io.on('connection', function (socket) {
     })
 });
 
-var leaveGame = function (socket) {
-    if (games[socket.gamename]) {
-        io.to(socket.gamename).emit('opponent_leave');
-        winner.leave(games[socket.gamename].name)
-        winner.game = null;
-    }
-};
+// var leaveGame = function (socket) {
+//     if (games[socket.gamename]) {
+//         io.to(socket.gamename).emit('opponent_leave');
+//         winner.leave(games[socket.gamename].name)
+//         winner.game = null;
+//     }
+// };
